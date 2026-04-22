68bd10eb7a49e0c75f0277005d7df8cbd3ceb898
junaid1068 <42918274+junaid1068@users.noreply.github.com>
2026-04-22 20:21:59 +0530

    Create Datawarehouse database with schemas
    This script creates a database named Datawarehouse and three schemas: bronze, silver, and gold. It will drop and recreate the database if it already exists.

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,1 +1,35 @@
+/*
+Script Purpose:
+Create a database named Datawarehouse and three schemas named bronze, silver, and gold.
 
+Warning : 
+Running this script will drop and recreate the entire 'Datawarehouse' database if it exists.
+
+*/
+Use Master;
+go
+if exists (Select 1 from sys.databases where name = 'Datawarehouse')
+Begin
+	Alter Database Datawarehouse Set Single_User With Rollback Immediate;
+	Drop Database Datawarehouse;
+End;
+go
+
+--Create Database and Schemas
+-- Schemas : Schema is a logical container for database objects such as tables, views, and stored procedures.
+--It helps to organize and manage database objects based on their purpose or function.
+-- It works like a folder structure within the database, allowing you to group related objects together.
+Create Database Datawarehouse ;
+go
+
+use Datawarehouse;
+go
+
+Create Schema Bronze;
+go
+
+Create Schema silver;	
+go
+
+Create Schema gold;
+go
